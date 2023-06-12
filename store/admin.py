adaac2d530b5c1a852b4f0a446f126b50d98a6b4
Joshua Gato <joshuagato37@gmail.com>
2023-06-12 09:48:04 +0000

    Added computed columns and selected related data

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,29 @@
 from django.contrib import admin
-from .models import Collection, Product
+from .models import Collection, Product, Customer
 
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'unit_price', 'inventory_status', 'collection_title')
+    list_editable = ('unit_price',)
+    list_per_page = 10
+    list_select_related = ['collection']
+
+    def collection_title(self, product):
+        return product.collection.title
+
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return 'Low'
+        return 'OK'
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'membership')
+    list_editable = ('membership',)
+    ordering = ('first_name', 'last_name')
+    list_per_page = 10
 
 admin.site.register(Collection)
-admin.site.register(Product)
