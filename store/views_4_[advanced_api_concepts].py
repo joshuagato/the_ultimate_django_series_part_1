f01dc5e42246aaffe7b785c84d9e72d6f0805c74
Joshua Gato <joshuagato37@gmail.com>
2023-06-18 15:39:19 +0000

    Renamed some files

--- store/views_4_[advanced_api_concepts].py
+++ store/views_4_[advanced_api_concepts].py
@@ -18,13 +18,26 @@ from .pagination import DefaultPagination
 
 
 class ProductVewSet(ModelViewSet):
+    # queryset = Product.objects.select_related('collection').all() # We didn't need this line due to the get_queryset function
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    # filterset_fields = ['collection_id'] # We use this attribute when we don't need to specify a filter class for specifying exact the behavior of filters
     filterset_class = ProductFilter
+    # pagination_class = PageNumberPagination # We don't need this after with define pagination globally
     pagination_class = DefaultPagination
     search_fields = ['title', 'description']
     ordering_fields = ['unit_price', 'last_update']
+
+
+    # def get_queryset(self):
+    #     # queryset = Product.objects.select_related('collection').all()
+    #     queryset = Product.objects.all()
+    #     collection_id = self.request.query_params.get('collection_id')
+    #     if collection_id is not None:
+    #         queryset = queryset.filter(collection_id=collection_id)
+
+    #     return queryset
 
 
     def get_serializer_context(self):
@@ -50,6 +63,9 @@ class CollectionViewSet(ModelViewSet):
 
 
 class ReviewViewSet(ModelViewSet):
+    #Here, we need to filter out for reviews that pertain to the product in question;
+    # hence we need to use the get_queryset method instead of the queryset property
+    # queryset = Review.objects.all()
     serializer_class = ReviewSerializer
 
     def get_queryset(self):
