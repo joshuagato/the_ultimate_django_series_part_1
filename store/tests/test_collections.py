510a5afce3f8218c5c8155a9485f8331cad5c7fd
Joshua Gato <joshuagato37@gmail.com>
2023-07-02 00:14:15 +0000

    Wrote tests and installed pytest-watch

---
+++ store/tests/test_collections.py
@@ -0,0 +1,18 @@
+from rest_framework import status
+from rest_framework.test import APIClient
+import pytest
+
+
+@pytest.mark.django_db
+class TestCreateCollection:
+    # AAA => Triple A
+    # @pytest.mark.skip
+    def test_if_user_is_anonymous_returns_401(self):
+        # Arrange
+
+        # Act
+        client = APIClient()
+        response = client.post('/store/collections/', { 'title': 'A' })
+
+        # Assert
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
