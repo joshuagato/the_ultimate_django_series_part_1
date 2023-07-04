954db6ad868e2797909f99099c5c6ef82aaab48a
Joshua Gato <joshuagato37@gmail.com>
2023-07-04 16:51:36 +0000

    Added locust for performance testing

---
+++ tips.py
@@ -0,0 +1,19 @@
+# Preload related objects
+Product.objects.select_related('...')
+Product.objects.prefetch_related('...')
+
+# Load only what you need
+Product.objects.only('title')
+Product.objects.defer('description')
+
+# Use values
+Product.objects.values()
+Product.objects.values_list()
+
+# Count properly
+Product.objects.count()
+len(Product.objects.all()) # BAD
+
+# Bulk create/update
+Product.objects.bulk_create([])
+Product.objects.bulk_update([])
